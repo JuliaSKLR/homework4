eb9a06b8ee98eef49346ec2fca88c0c20c275806
Julia.S <der.lavender@gmail.com>
2022-08-17 18:45:47 +0300

    001

---
+++ 001/Program.cs
@@ -0,0 +1,19 @@
+﻿// 1: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int A = Prompt("Введите число А ");
+int B = Prompt("Введите число B ");
+int result = A;
+for (int i =1; i<B;  i++ ){
+    result = result*A;
+}
+System.Console.WriteLine(result);
